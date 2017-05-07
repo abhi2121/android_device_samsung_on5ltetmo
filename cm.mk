03d98fe18702370c102defee37dba22d822f71dd
abhi2121 <mylovesonu37@gmail.com>
2017-05-07 10:41:22 +0530

    Update and rename omni_on5ltetmo.mk to cm.mk

--- cm.mk
+++ cm.mk
@@ -4,10 +4,10 @@ PRODUCT_RELEASE_NAME := on5ltetmo
 $(call inherit-product, build/target/product/embedded.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/cm/config/common.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := on5ltetmo
-PRODUCT_NAME := omni_on5ltetmo
+PRODUCT_NAME := cm_on5ltetmo
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
